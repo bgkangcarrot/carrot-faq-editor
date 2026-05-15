94963ea98cf93526b02263687f2f56d8e209c22e
bgkangcarrot <bgkangcarrot@gmail.com>
2026-05-15 23:26:40 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // ★ 아래 두 값을 Supabase 대시보드 Settings > API 에서 복사해서 교체하세요
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://zpvkqgjttqvasriwcttu.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_QcfzbwPgrFh-RTYyrub8Wg_iEWe2t9T;
 
 const { createClient } = supabase;
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
